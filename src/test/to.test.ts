104734d17c8079edae07ef16043908bf61ef8fd2
momotom <a18520145493@163.com>
2025-08-07 20:19:50 +0800

    test: to => 解析失败时应返回错误

--- src/test/to.test.ts
+++ src/test/to.test.ts
@@ -1,11 +1,18 @@
 import { to } from '@lib/index'
 import { describe, it, expect } from 'vitest'
 
-
 describe('to', () => {
-  it('should return a value when resolved', async () => {
+  it('解析时应返回值', async () => {
     const [err, data] = await to(Promise.resolve('test'))
     expect(err).toBeNull()
     expect(data).toBe('test')
+  })
+  it('解析失败时应返回错误', async () => {
+    const promise = Promise.reject('Error')
+
+    const [err, data] = await to(promise)
+    expect(err).toBe('Error')
+    expect(data).toBeUndefined()
+    
   })
 })
