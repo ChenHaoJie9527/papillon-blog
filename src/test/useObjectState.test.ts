b2dda9ba4fa43a8eb8755de4facee32605b60134
momotom <a18520145493@163.com>
2025-08-08 17:25:53 +0800

    test: useObjectState => 应该通过函数更新状态

--- src/test/useObjectState.test.ts
+++ src/test/useObjectState.test.ts
@@ -35,4 +35,22 @@ describe('useObjectState', () => {
       age: 11,
     })
   })
+  it('应该通过函数更新', () => {
+    const initialState = { name: 'Join', age: 10 }
+    const { result } = renderHook(() => useObjectState(initialState))
+
+    act(() => {
+      const [, setState] = result.current
+      setState((s: typeof initialState) => ({
+        ...s,
+        age: s.age + 10,
+      }))
+    })
+
+    const [state] = result.current
+    expect(state).toEqual({
+      name: 'Join',
+      age: 20,
+    })
+  })
 })
