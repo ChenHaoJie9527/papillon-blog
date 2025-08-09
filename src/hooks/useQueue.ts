0cc34deb7108770b0ac8affc01f728cd1264e7fe
momotom <a18520145493@163.com>
2025-08-09 22:14:36 +0800

    feat: 1.创建useQueue用于操作队列的hook; 2.添加add方法

---
+++ src/hooks/useQueue.ts
@@ -0,0 +1,23 @@
+import { useCallback, useState } from 'react'
+
+/**
+ * 操作队列hook
+ * @param initialValue 初始值，接受一个数组，可以说任意类型的数组
+ * @returns add 添加元素方法
+ * @returns remove 删除元素方法
+ * @returns clear 清空队列方法
+ * @return at 获取队列中指定位置的元素方法
+ * @returns queue 当前队列
+ */
+export function useQueue<T>(initialValue: readonly T[]) {
+  const [queue, setQueue] = useState(initialValue)
+
+  const add = useCallback((element: T) => {
+    setQueue((s) => [...s, element])
+  }, [])
+
+  return {
+    queue,
+    add,
+  }
+}
